b23e248466b7ce322b67aeaf9ebd585438c852b4
suhas023 <notsuhas@gmail.com>
2018-08-15 16:58:37 +0530

    routing strategy uses hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes:Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    RouterModule.forRoot(routes)
+    RouterModule.forRoot(routes, {useHash: true})
   ],
   declarations: [],
   exports: [ RouterModule ]
